72aa98ba18ab76355432fd3a56e1e103f2eaf4e4
PetzJohannes <johannes.petz@hotmail.de>
2016-04-02 20:31:05 +0200

    latest data format item names with key values

--- js/actions/item.js
+++ js/actions/item.js
@@ -6,6 +6,17 @@ define(['jquery', 'zabbix'], function( $, zabbix ) {
                 monitored: true
             };
             zabbix.tableLoad(params, "item.get", paramszapi);
+        },
+        formatItemValue: function (value, key) {
+            if (/\$[0-9]*/.test(value)) {
+                var position = (/\$[0-9]*/.exec(value))[0].substr(1);
+                //var keyvalues = (/\[([^\]]+)\]/.exec(key))[0];
+                var keyvalues = key.match(/\[(.*?)\]/)[1].split(",");
+                console.log(position);
+                console.log(keyvalues);
+                value = value.replace("$" + position, keyvalues[position-1]);
+            }
+            return value;
         }
     }
 });
