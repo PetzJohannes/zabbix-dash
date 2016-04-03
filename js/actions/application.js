a0fb3aaef456766e739350cbb2472d8d8a6dd900
PetzJohannes <johannes.petz@hotmail.de>
2016-04-03 21:09:47 +0200

    smaller ajax now

--- js/actions/application.js
+++ js/actions/application.js
@@ -2,6 +2,7 @@ define(['jquery', 'zabbix'], function( $, zabbix ) {
     return {
         getHostApplications: function (params, hostid) {
             var paramszapi = {
+                output: ["name"],
                 hostids: hostid
             };
             zabbix.tableLoad(params, "application.get", paramszapi);
