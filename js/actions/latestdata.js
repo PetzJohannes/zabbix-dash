a31b80384236997fecd9017095a45ea12e71721b
PetzJohannes <johannes.petz@hotmail.de>
2016-04-03 16:03:01 +0200

    latest data view timestamp to human date

--- js/actions/latestdata.js
+++ js/actions/latestdata.js
@@ -9,7 +9,7 @@ define(['jquery', 'zabbix'], function( $, zabbix ) {
                 '>' +
                 '<thead><tr>' +
                 '<th data-field="name" data-formatter="formatItemName">Name</th>' +
-                '<th data-field="lastclock">Last Check</th>' +
+                '<th data-field="lastclock" data-formatter="getHumanTime">Last Check</th>' +
                 '<th data-field="lastvalue" data-formatter="formatItemValue">Last Value</th>' +
                 '<th data-field="state" data-width="2%" data-align="center" data-formatter="formatItemState">Status</th>' +
                 '</tr></thead></table>');
